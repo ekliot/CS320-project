b91535e968c3b4a91dc80a508587ee276a72ecef
Matthew Witte <mcw9612@rit.edu>
2017-03-31 13:12:30 -0400

    Creating a user

---
+++ src/main/java/rpgcharacters/commands/CommandCreateUser.java
@@ -0,0 +1,39 @@
+package rpgcharacters.commands;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+@Parameters(commandDescription = "Create a user")
+public class CommandCreateUser implements Command {
+    @Parameter(names = "--username",
+               description = "Username of the user to create",
+               required = true)
+    private String username;
+    
+    @Parameter(names = "--password",
+               description = "Password of the user to create",
+               required = true)
+    private String password;
+
+    @Override
+    public void run(Connection conn) {
+        try {
+            String query = "INSERT INTO user VALUES ("
+                         + "'" + username + "'"
+                         + ","
+                         + "'" + password + "'"
+                         + ");";
+
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+            System.out.println("Welcome, " + username + "!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
