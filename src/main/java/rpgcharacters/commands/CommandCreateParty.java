6621fa6196ad683bf8e6d7f19fc00ca43950915e
Alberto Serrano <ascalva@gmail.com>
2017-04-07 21:09:15 -0400

    Add files via upload

---
+++ src/main/java/rpgcharacters/commands/CommandCreateParty.java
@@ -0,0 +1,33 @@
+package rpgcharacters.commands;
+
+import java.sql.*;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+@Parameters(commandDescription = "Create a party")
+public class CommandCreateParty implements Command {
+    @Parameter(names = "--name",
+            description = "Name of new party",
+            required = true)
+    private String name;
+
+    @Parameter(names = "--gm",
+    description = "Name of user acting as the game master for the party",
+    required = true)
+    private String gm;
+
+    public void run(Connection connection {
+        try {
+            String query = "INSEERT * INTO party ("
+                         + "'" + this.name + "'"
+                         + "'" + this.name + "'"
+                         + "'" + this.gm + "'";
+
+            Statement st = connection.createStatement();
+            st.executeQuery(query);
+        } catch( SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
