560ccbf0c9215e333ba604f42244ad5d9ef7995b
Alberto Serrano <ascalva@gmail.com>
2017-04-08 21:05:43 -0400

    Class name and query changes

--- src/main/java/rpgcharacters/commands/CommandRemoveFromParty.java
+++ src/main/java/rpgcharacters/commands/CommandRemoveFromParty.java
@@ -6,13 +6,13 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
 @Parameters(commandDescription = "Delete a character from a party")
-public class CommandDeleteParty implements Command {
+public class CommandRemoveFromParty implements Command {
 
     @Parameter(names = "--name",
-    description = "Name of character to delete from party",
+            description = "Name of character to delete from party",
             required = true)
     private String name;
-    
+
     @Parameter(names = "--username",
             description = "",
             required = true)
@@ -24,7 +24,8 @@ public class CommandDeleteParty implements Command {
             String query = "SELECT * "
                     +"FROM character "
                     + "WHERE name = '" + this.name
-                    + "' AND user_name ='" + this.username + "';";
+                    + "' AND user_name ='" + this.username + "'"
+                    + "AND party_id IS NOT NULL;";
 
             Statement st = connection.createStatement();
             ResultSet results = st.executeQuery(query);
@@ -36,14 +37,14 @@ public class CommandDeleteParty implements Command {
             } else {
                 query = "UPDATE character "
                         + "SET party_id = NULL "
-                        + "WHERE character.name ='" + this.name + "';";
-                
+                        + "WHERE character.name ='" + this.name + "'"
+                        + "AND character.user_username ='" + this.name + "';";
                 Statement rm = connection.createStatement();
                 rm.execute(query);
                 System.out.format("Character %s has been successfully deleted from the party.\n",
                         this.name);
             }
-        } catch( SQLException e) {
+        } catch( SQLException e ) {
             e.printStackTrace();
         }
     }
