befcc78129825315901554ba1f78227c4b2b107f
Alberto Serrano <ascalva@gmail.com>
2017-04-09 17:17:55 -0400

    Rename of command

--- src/main/java/rpgcharacters/RpgCharacters.java
+++ src/main/java/rpgcharacters/RpgCharacters.java
@@ -33,9 +33,8 @@ public class RpgCharacters {
         commands.put("create-character", new CommandCreateCharacter());        
         commands.put("create-item", new CommandCreateItem());
         commands.put("create-quest", new CommandCreateQuest());
-        commands.put("list-quests", new CommandListQuests());
         commands.put("add-party", new CommandAddParty());
-        commands.put("removeFrom-party", new CommandRemoveFromParty());
+        commands.put("remove-from-party", new CommandRemoveFromParty());
         commands.put("create-party", new CommandCreateParty());
         commands.put("list-party", new CommandListParties());
         commands.put("delete-quest", new CommandDeleteQuest());
